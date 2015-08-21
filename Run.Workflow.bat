c0e08d63aad91525cce7d360e6ab7892b2f24110
alhails <alastair.hails@intelliflo.com>
2015-08-21 17:20:35 +0100

    Fix namespaces

--- Run.Workflow.bat
+++ Run.Workflow.bat
@@ -1,5 +1,5 @@
 TITLE Workflow
 
-msbuild %~dp0src\IntelliFlo.Platform.Services.Workflow\IntelliFlo.Platform.Services.Workflow.csproj /p:PostBuildEvent=
+msbuild %~dp0src\Microservice.Workflow\Microservice.Workflow.csproj /p:PostBuildEvent=
 
-Call %~dp0src\IntelliFlo.Platform.Services.Workflow\bin\Debug\Microservice.Workflow.exe
+Call %~dp0src\Microservice.Workflow\bin\Debug\Microservice.Workflow.exe
--- src/Microservice.Workflow/Properties/AssemblyInfo.cs
+++ src/Microservice.Workflow/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MicroService.Workflow")]
+[assembly: AssemblyTitle("Microservice.Workflow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("..")]
-[assembly: AssemblyProduct("MicroService.Workflow")]
+[assembly: AssemblyProduct("Microservice.Workflow")]
 [assembly: AssemblyCopyright("Copyright © .. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
