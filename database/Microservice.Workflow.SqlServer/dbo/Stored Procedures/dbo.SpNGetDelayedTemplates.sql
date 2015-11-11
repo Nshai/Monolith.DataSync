da4c89c10407fe634654c992bf6b1b518bf5376f
alhails <alastair.hails@intelliflo.com>
2015-11-11 09:44:36 +0000

    Reduce blocking for delayed templates query

--- database/Microservice.Workflow.SqlServer/dbo/Stored Procedures/dbo.SpNGetDelayedTemplates.sql
+++ database/Microservice.Workflow.SqlServer/dbo/Stored Procedures/dbo.SpNGetDelayedTemplates.sql
@@ -12,7 +12,7 @@ DateUtc,
 from
 WF_TTemplateDefinition 
 where Id in (select distinct t.Id
-from vwInstanceStep s
+from vwInstanceStep s with (nolock)
 inner join WF_TInstance i on s.InstanceId = i.Id
 inner join WF_TTemplateDefinition t on i.TemplateId = t.Id
 where s.IsComplete = 0 and s.Step = 'Delay' and t.Version >= @Version)
