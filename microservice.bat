8980d98efa46d7c6c38c7cf56cd378b0c97a573e
alhails <alastair.hails@intelliflo.com>
2015-12-04 11:01:53 +0000

    Amend build files for TeamCity

--- microservice.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild" microservice.proj /target:Version /p:RunCodeAnalysis=false;Build=1.2.3;GitHash=033ab5c90e3422204b5c576dfa2db4359dde0d65;TargetFrameworkVersion=v4.5;Configuration=Release /logger:FileLogger,Microsoft.Build.Engine;logfile=teamcityLog.log
-pause
---
+++ version.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild" version.targets /target:Version /p:RunCodeAnalysis=false;Build=1.2.3;GitHash=091e588f04c1d67ed255a3d2891a651f1d2ee29c;TargetFrameworkVersion=v4.5.1;Configuration=Release /logger:FileLogger,Microsoft.Build.Engine;logfile=teamcityLog.log
+pause
