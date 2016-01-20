Nshai/Monolith.DataSync

commit af151d6c54fd923e2558501ea560ab7146280e1d

Hide system instances from GET v1/instances

src/Microservice.Workflow/v1/Resources/InstanceResource.cs
@@ -51,13 +51,14 @@ namespace Microservice.Workflow.v1.Resources
 
         public PagedResult<InstanceDocument> Query(string query, IDictionary<string, object> routeValues)
         {
+            var tenantId = Thread.CurrentPrincipal.AsIFloPrincipal().TenantId;
             ICriterion[] additionalCriteria =
             {
-                Restrictions.Eq("TenantId", (int) Thread.CurrentPrincipal.AsIFloPrincipal().TenantId)
+                Restrictions.And(Restrictions.Eq("TenantId", tenantId), Restrictions.Eq("t.TenantId", tenantId))
             };
 
             int count;
-            var instances = instanceRepository.ODataQueryWithInlineCount(query, out count, additionalCriteria);
+            var instances = instanceRepository.ODataQueryWithInlineCount(query, out count, new Dictionary<string, string>() {{"Template", "t"}}, additionalCriteria);
 
             return new PagedResult<InstanceDocument>
             {

commit 7a652eb65a1a26b9c66daf95345969c1826d6bfe

IP-1214 Fix instance filtering
Filtering was regressed by commit af151d6 which added NH query aliases.
NH Odata doesn't seem to play well when you have defined aliases and
calls querying on the aliased property fail.

As a workaround, moved to view and added TemplateTenantId property that
avoids the need to add alias.

This is possibly fixed in later version of Odata library, but we
currently are unable to upgrade as it would require an NH upgrade also.

database/Microservice.Workflow.SqlServer/Microservice.Workflow.SqlServer.sqlproj
@@ -123,6 +123,7 @@
     <None Include="dbodata\Data\main\4.4-000130 IO-42821 Update old instances to reference active template version.sql" />
     <None Include="dbodata\Data\main\4.5-000010 IO-46251 Author system workflows.sql" />
     <Build Include="dbo\Tables\dbo._EventLog.sql" />
+    <Build Include="dbo\Views\dbo.vwInstance.sql" />
   </ItemGroup>
   <PropertyGroup>
     <PostBuildEvent>PowerShell -NoProfile -ExecutionPolicy Bypass -Command "&amp; '$(SolutionDir)packages\IntelliFlo.PowerShell.DevLib.0.0.3.0\content\zip-directory.ps1' -ZIPFILENAME '$(SolutionDir)dist\db-workflow.zip' -SOURCEDIR '$(ProjectDir)dbo\'

database/Microservice.Workflow.SqlServer/dbo/Views/dbo.vwInstance.sql
@@ -1,0 +1,21 @@
+﻿create view [dbo].[vwInstance]
+  as
+  SELECT
+	  i.Id, 
+	  i.TemplateId, 
+	  i.CorrelationId, 
+	  i.UserId, 
+	  i.TenantId, 
+	  t.TenantId 'TemplateTenantId',
+	  i.EntityId, 
+	  i.EntityType, 
+	  i.RelatedEntityId, 
+	  i.ParentEntityType,
+	  i.ParentEntityId, 
+	  i.CreatedUtc, 
+	  i.[Status], 
+	  i.UniqueId, 
+	  i.[Version]
+  FROM [WF_TInstance] i
+  INNER JOIN [WF_TTemplateDefinition] t on i.TemplateId = t.Id  
+GO

src/Microservice.Workflow/Domain/Instance.cs
@@ -8,6 +8,7 @@ namespace Microservice.Workflow.Domain
         public virtual int UserId { get; set; }
         public virtual int TenantId { get; set; }
         public virtual TemplateDefinition Template { get; set; }
+        protected virtual int TemplateTenantId { get; set; }
         public virtual string EntityType { get; set; }
         public virtual int EntityId { get; set; }
         public virtual string ParentEntityType { get; set; }

src/Microservice.Workflow/Domain/Instance.hbm.xml
@@ -6,13 +6,14 @@
 									 default-access="property"
                    schema="workflow.dbo">
 
-  <class name="Instance" table="WF_TInstance">
+  <class name="Instance" table="vwInstance">
 
     <id name="Id">
       <generator class="assigned"/>
     </id>
     
     <many-to-one name="Template" class="TemplateDefinition" column="TemplateId" not-null="true" lazy="false" not-found="ignore"/>
+    <property name="TemplateTenantId" insert="false" update="false"/>
     <property name="CorrelationId"/>
     <property name="UserId" />
     <property name="TenantId" />

src/Microservice.Workflow/v1/Resources/InstanceResource.cs
@@ -54,11 +54,11 @@ namespace Microservice.Workflow.v1.Resources
             var tenantId = Thread.CurrentPrincipal.AsIFloPrincipal().TenantId;
             ICriterion[] additionalCriteria =
             {
-                Restrictions.And(Restrictions.Eq("TenantId", tenantId), Restrictions.Eq("t.TenantId", tenantId))
+                Restrictions.And(Restrictions.Eq("TenantId", tenantId), Restrictions.Eq("TemplateTenantId", tenantId))
             };
 
             int count;
-            var instances = instanceRepository.ODataQueryWithInlineCount(query, out count, new Dictionary<string, string>() {{"Template", "t"}}, additionalCriteria);
+            var instances = instanceRepository.ODataQueryWithInlineCount(query, out count, additionalCriteria);
 
             return new PagedResult<InstanceDocument>
             {