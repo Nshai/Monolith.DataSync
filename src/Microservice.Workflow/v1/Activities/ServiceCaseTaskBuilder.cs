fe6effb498660d99dd40a87ce366bb740af8bb9e
YURY HERDZEI <y.herdzei@godeltech.com>
2017-04-11 12:20:41 +0300

    Joint client is defined depending on the client's context when a task is created for service case.

--- src/Microservice.Workflow/v1/Activities/ServiceCaseTaskBuilder.cs
+++ src/Microservice.Workflow/v1/Activities/ServiceCaseTaskBuilder.cs
@@ -23,14 +23,18 @@ namespace Microservice.Workflow.v1.Activities
         {
             var serviceCase = await GetServiceCase(context.ClientId, context.EntityId);
 
-            var owner1 = serviceCase.ClientId;
-            var owner2 = serviceCase.JointClientId;
+            var owner1Id = serviceCase.ClientId;
+            var owner2Id = serviceCase.JointClientId;
             request.RelatedPartyId = context.ClientId;
             request.RelatedServiceCaseId = context.EntityId;
-            if (owner2.HasValue)
-                request.RelatedJointPartyId = owner2.Value;
 
-            return owner1;
+            if (owner2Id != null)
+            {
+                // NB ClientId may be joint owner (rather than primary)
+                var jointOwnerId = owner2Id != context.ClientId ? owner2Id : owner1Id;
+                request.RelatedJointPartyId = jointOwnerId;
+            }
+            return owner1Id;
         }
 
         private async Task<ServiceCaseDocument> GetServiceCase(int clientId, int serviceCaseId)
