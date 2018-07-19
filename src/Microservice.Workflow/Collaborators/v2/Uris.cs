fe65ef834830c720922009610e78ebe0d0349f87
Egor Sidorovich <e.sidorovich@godeltech.com>
2018-07-19 17:29:29 +0300

    IP-39173 Workflow changes for ServicingAdministrator && Paraplanner context roles usage
    IP-39173: Refactoring
    
    IP-39173: Upgrade  platform packages
    
    IP-39173: Limit ClientTaskBuilder to handle only specific context roles

---
+++ src/Microservice.Workflow/Collaborators/v2/Uris.cs
@@ -0,0 +1,11 @@
+﻿namespace Microservice.Workflow.Collaborators.v2
+{
+    public static class Uris
+    {
+        public static class Crm
+        {
+            public const string GetClient = "v2/clients/{0}";
+        }
+
+    }
+}
--- src/Microservice.Workflow/v1/Activities/ClientTaskBuilder.cs
+++ src/Microservice.Workflow/v1/Activities/ClientTaskBuilder.cs
@@ -1,8 +1,13 @@
-﻿using System.Activities;
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using IntelliFlo.Platform.Http.Client;
 using IntelliFlo.Platform.Http.Client.Policy;
-using Microservice.Workflow.Collaborators.v1;
+using V1 = Microservice.Workflow.Collaborators.v1;
+using V2 = Microservice.Workflow.Collaborators.v2;
+using Microservice.Workflow.Domain;
+using Constants = IntelliFlo.Platform.Principal.Constants;
 
 namespace Microservice.Workflow.v1.Activities
 {
@@ -12,25 +17,72 @@ namespace Microservice.Workflow.v1.Activities
 
         public async override Task<int> GetContextPartyId(string ownerContextRole, WorkflowContext context)
         {
-            if (ownerContextRole != "ServicingAdviser")
+            RoleContextType contextRole;
+
+            if (!Enum.TryParse(ownerContextRole, out contextRole))
+            {
                 return PartyNotFound;
+            }
 
             using (var crmClient = ClientFactory.Create("crm"))
             {
-                var clientResponse = await crmClient.UsingPolicy(HttpClientPolicy.Retry).SendAsync(c => c.Get<ClientDocument>(string.Format(Uris.Crm.GetClient, context.EntityId)));
-                clientResponse.OnException(s => { throw new HttpClientException(s); });
-                var client = clientResponse.Resource;
+                switch (contextRole)
+                {
+                    case RoleContextType.ServicingAdviser:
+                    {
+                        var clientResponseV1 = await crmClient.UsingPolicy(HttpClientPolicy.Retry)
+                                                            .SendAsync(c => c.Get<V1.ClientDocument>(string.Format(V1.Uris.Crm.GetClient, context.EntityId)))
+                                                            .OnException(s => { throw new HttpClientException(s); });
 
-                return client.CurrentAdviserPartyId;
+                        var clientDocumentV1 = clientResponseV1.Resource;
+                        return clientDocumentV1.CurrentAdviserPartyId;
+                    }
+                    case RoleContextType.ServicingAdministrator:
+                    case RoleContextType.Paraplanner:
+                    {
+                        var clientResponseV2 = await crmClient.UsingPolicy(HttpClientPolicy.Retry)
+                                                              .SendAsync(c => c.Get<V2.ClientDocument>(string.Format(V2.Uris.Crm.GetClient, context.EntityId)))
+                                                              .OnException(s => { throw new HttpClientException(s); });
+
+                        var clientDocumentV2 = clientResponseV2.Resource;
+
+                        var entityId = GetEntityId(contextRole, clientDocumentV2);
+
+                        if (!entityId.HasValue)
+                        {
+                            return PartyNotFound;
+                        }
+
+                        var userClaimsResponse = await crmClient.UsingPolicy(HttpClientPolicy.Retry)
+                                                                .SendAsync(c => c.Get<Dictionary<string, object>>(string.Format(V1.Uris.Crm.GetUserInfoByUserId, entityId)))
+                                                                .OnException(s => { throw new HttpClientException(s); });
+
+                        var userClaims = userClaimsResponse.Resource;
+
+                        return userClaims.ContainsKey(Constants.ApplicationClaimTypes.PartyId)
+                            ? int.Parse(userClaims[Constants.ApplicationClaimTypes.PartyId].ToString())
+                            : PartyNotFound;
+                    }
+                    default:
+                        return PartyNotFound;
+                }
             }
         }
 
-        public async override Task<int> PrepareRequest(CreateTaskRequest request, WorkflowContext context)
+        public async override Task<int> PrepareRequest(V1.CreateTaskRequest request, WorkflowContext context)
         {
             request.RelatedPartyId = context.EntityId;
             if (context.RelatedEntityId > 0)
                 request.RelatedJointPartyId = context.RelatedEntityId;
             return context.EntityId;
+        }
+
+        private int? GetEntityId(RoleContextType contextRole, V2.ClientDocument client)
+        {
+            return contextRole == RoleContextType.ServicingAdministrator
+                ? client.ServicingAdministrator?.Id
+                : client.Paraplanner?.Id;
+
         }
     }
 }
