63f3faf771f4f6be4feac112bf1fed016bb18ec1
YURY HERDZEI <y.herdzei@godeltech.com>
2018-11-20 10:39:30 +0300

    IP-43710 Workflow aborted by system with error

--- src/Microservice.Workflow/Domain/AbortRequestLog.cs
+++ src/Microservice.Workflow/Domain/AbortRequestLog.cs
@@ -5,12 +5,13 @@ namespace Microservice.Workflow.Domain
     public class AbortRequestLog : ILogDetail, IEquatable<AbortRequestLog>
     {
         public int UserRequested { get; set; }
+        public string Reason { get; set; }
 
         public bool Equals(AbortRequestLog other)
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return UserRequested == other.UserRequested;
+            return UserRequested == other.UserRequested && Reason == other.Reason;
         }
 
         public override bool Equals(object obj)
