368b122c26e9905f02f6b9ee01d2416df1436487
YURY HERDZEI <y.herdzei@godeltech.com>
2020-09-03 17:37:36 +0300

    IP-81593 Workflow is failing - System error occurs (#79)

--- src/Microservice.Workflow/HttpClientPolicy.cs
+++ src/Microservice.Workflow/HttpClientPolicy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using IntelliFlo.Platform.Http.Client;
@@ -8,6 +9,12 @@ namespace Microservice.Workflow
 {
     public static class HttpClientPolicy
     {
+        private static List<HttpStatusCode> retryable = new List<HttpStatusCode>
+        {
+            HttpStatusCode.ServiceUnavailable,
+            HttpStatusCode.InternalServerError
+        };
+
         public static Policy Retry
         {
             get
@@ -15,6 +22,17 @@ namespace Microservice.Workflow
                 return Policy
                     .Handle<TaskCanceledException>()
                     .Or<HttpStatusException>(e => e.StatusCode == HttpStatusCode.ServiceUnavailable)
+                    .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(10 * i));
+            }
+        }
+
+        public static Policy RetryOn500Error
+        {
+            get
+            {
+                return Policy
+                    .Handle<TaskCanceledException>()
+                    .Or<HttpStatusException>(e => retryable.Contains(e.StatusCode))
                     .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(10 * i));
             }
         }
--- src/Microservice.Workflow/v1/Activities/EntityTaskBuilder.cs
+++ src/Microservice.Workflow/v1/Activities/EntityTaskBuilder.cs
@@ -107,7 +107,8 @@ namespace Microservice.Workflow.v1.Activities
                 // Override the default 5 second timeout
                 crmClient.Timeout = TimeSpan.FromMinutes(2);
 
-                var taskResponse = await crmClient.UsingPolicy(HttpClientPolicy.Retry).SendAsync(c => c.Post<TaskDocument, CreateTaskRequest>(Uris.Crm.CreateTask, request));
+                //if CRM can't create a task due to DB slowness it will return 500 error as crmClient.Timeout = 2 mins which is > 30 secs DB timeout in CRM.
+                var taskResponse = await crmClient.UsingPolicy(HttpClientPolicy.RetryOn500Error).SendAsync(c => c.Post<TaskDocument, CreateTaskRequest>(Uris.Crm.CreateTask, request));
                 taskResponse.OnException(s => { throw new HttpClientException(s); });
                 return taskResponse.Resource;
             }
